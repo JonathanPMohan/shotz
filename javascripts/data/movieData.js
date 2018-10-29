b95ba86f0a27ba83d4dc9df9d27e9eec00f74976
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-10-29 16:41:29 -0500

    Fixed a lot, have a lot messed up still but need to work it out.

--- javascripts/data/movieData.js
+++ javascripts/data/movieData.js
@@ -1,13 +1,27 @@
 import { filmBuilder } from "../components/movieComponent.js";
 
+// const filmInfo = () => {
+//     $.get('../db/movie.json')
+//         .done((data) => {
+//             filmBuilder(data.movie);
+//         })
+//         .fail((error) => {
+//             console.log(error);
+//         });
+// }
+
 const filmInfo = () => {
-    $.get('../db/movie.json')
-        .done((data) => {
-            filmBuilder(data.movie);
-        })
-        .fail((error) => {
-            console.log(error);
-        });
+    return new Promise((resolve, reject) => {
+        $.get('../db/movie.json')
+            .done(data => {
+                resolve(data.movie);
+                filmBuilder(data.movie);
+            })
+            .fail(error => {
+               reject(error);
+            })
+    });
+
 }
 
 export { filmInfo };
